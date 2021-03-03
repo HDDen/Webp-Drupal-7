742e1239345274b8bbee08fc3c4fd4c426678e5d
Den <me@hdden.ru>
2021-03-03 11:38:40 +0300

    - Updated DiDOM to 1.16.4

--- webpdrupal7/core_scripts/libs/DiDom/vendor/composer/autoload_real.php
+++ webpdrupal7/core_scripts/libs/DiDom/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit96a8c7bf3f0a8d1360b680e24bb21856
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit96a8c7bf3f0a8d1360b680e24bb21856', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit96a8c7bf3f0a8d1360b680e24bb21856', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit96a8c7bf3f0a8d1360b680e24bb21856::getInitializer($loader));
         } else {
--- webpdrupal7/core_scripts/libs/WebpConvert/vendor/composer/autoload_real.php
+++ webpdrupal7/core_scripts/libs/WebpConvert/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitf712046a5e220f56d496862fd5ec2a77
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf712046a5e220f56d496862fd5ec2a77', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf712046a5e220f56d496862fd5ec2a77', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf712046a5e220f56d496862fd5ec2a77::getInitializer($loader));
         } else {
